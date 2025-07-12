fe4943d6840c394ec85e649653e1581df2ba43fd
Mohd Anas Khan <119879801+anaskhaann@users.noreply.github.com>
2025-07-12 14:00:29 +0530

    Added Portfolio Data

---
+++ src/data/portfolioData.ts
@@ -0,0 +1,109 @@
+import type { Experience, Project, SkillCategory } from "@/types";
+
+export const experiences: Experience[] = [
+  {
+    id: 1,
+    title: " Front End Developer Intern",
+    company: " Farsoft Infotech Pvt. Ltd.",
+    duration: "Dec 2023 - March 2024",
+    description:
+      "Developed responsive websites using HTML, CSS, and JavaScript, boosting mobile engagement by 15%. Customized and launched 5+ sites while maintaining brand consistency and cross-browser compatibility. Collaborated with cross-functional teams to enhance performance and resolve technical issues.",
+    technologies: ["HTML", "CSS", "Javascript", "Bootstrap"],
+  },
+  {
+    id: 2,
+    title: " Machine Learning & Data Science Intern",
+    company: " PHNTechnology Pvt. Ltd.",
+    duration: "April 2023 - June 2023",
+    description:
+      "Gained hands-on experience in predictive modeling and data analysis through mentorship and project work. Contributed to building and validating a machine learning model using trading data. Created visualizations with Pandas and Matplotlib to support performance review and identify trading pattern improvements.",
+    technologies: ["Python", "Numpy", "Pandas", "Matplotlib"],
+  },
+];
+
+export const projects: Project[] = [
+  {
+    id: 1,
+    title: "Chat with Database",
+    description:
+      "Developed an AI tool enabling natural language SQL queries using LangChain, Streamlit, and Meta's LLaMA, allowing non-technical users to interact with MySQL and SQLite databases. Improved performance with caching and fine-tuned model parameters, reducing response times by 15-20% and enhancing query accuracy.",
+    technologies: ["Python", "Langchain", "Streamlit", "LLama", "MySQL"],
+    githubUrl: "https://github.com/anaskhaann/Chat-with-Database-SQL",
+    // liveUrl: "https://demo.com",
+    videoUrl:
+      "https://drive.google.com/file/d/1_zDUrBSChOOdiV0xt4BLuEMJCrTBH9nL/view?usp=sharing",
+    imageUrl: "/assets/projects/sample.png",
+  },
+  {
+    id: 2,
+    title: "Gesture Based Presentation Controller",
+    description:
+      "Developed a Gesture-Based Presentation system using Python, OpenCV, and MediaPipe,enabling touch-free slide navigation and annotation for intuitive presentation experience.Achieved 85% hand detection accuracy and 90% gesture recognition accuracy, with optimized slide transition under 0.5s, ensuring fast and reliable real-time control",
+    technologies: ["Python", "MediaPipe", "OpenCV", "Numpy"],
+    githubUrl: "https://github.com/anaskhaann/Gesture-Based-Presentation",
+    imageUrl: "/assets/projects/sample.png",
+  },
+  {
+    id: 3,
+    title: "Full Stack Web App",
+    description:
+      "Developed a Pinterest clone with Express.js and MongoDB, focusing on core social media features like board management, saving and deleting pins, and personalized feeds. Implemented file uploads with Multer, and used Mongoose for database interaction to streamline development.",
+    technologies: ["Javascript", "CSS", "Express.Js", "HTML"],
+    githubUrl: "https://github.com/anaskhaann/Pinterest-Clone",
+    imageUrl: "/assets/projects/sample.png",
+  },
+  {
+    id: 4,
+    title: "Web Automation With Selenium",
+    description:
+      "This Project Is Based On My Repo Named Daily Life Journal. This is Made to Automate its Printing Task and Create a Book.Automate webpage-to-PDF conversion with ease! Perfect for batch downloading and archiving web content.",
+    technologies: ["Python", "Selenium"],
+    liveUrl: "www.google.com",
+    videoUrl: "www.youtube.com",
+    githubUrl: "https://github.com/anaskhaann/Web-Automation-With-Selenium",
+    imageUrl: "/assets/projects/sample.png",
+  },
+];
+
+export const skillCategories: SkillCategory[] = [
+  {
+    category: "Languages",
+    skills: [
+      { name: "Python", icon: "/assets/skills/python.svg" },
+      { name: "JavaScript", icon: "/assets/skills/js.svg" },
+      { name: "HTML", icon: "/assets/skills/html.svg" },
+      { name: "CSS", icon: "/assets/skills/css.svg" },
+      { name: "Bash/Script", icon: "/assets/skills/bash.svg" },
+    ],
+  },
+  {
+    category: "Framworks",
+    skills: [
+      { name: "Git", icon: "/assets/skills/git.svg" },
+      { name: "Flask", icon: "/assets/skills/flask.svg" },
+      { name: "Jupyter", icon: "/assets/skills/jupyter.svg" },
+      { name: "Numpy", icon: "/assets/skills/numpy.svg" },
+      { name: "Pandas", icon: "/assets/skills/pandas.svg" },
+      { name: "Matplotlib", icon: "/assets/skills/matplotlib.svg" },
+    ],
+  },
+  {
+    category: "Database",
+    skills: [
+      { name: "MySQL", icon: "/assets/skills/mysql.svg" },
+      { name: "SQlite", icon: "/assets/skills/sqlite.svg" },
+      { name: "MongoDB", icon: "/assets/skills/mongodb.svg" },
+    ],
+  },
+  {
+    category: "Others",
+    skills: [
+      { name: "Vs Code", icon: "/assets/skills/vscode.svg" },
+      { name: "Linux", icon: "/assets/skills/linux.svg" },
+      { name: "GitHub", icon: "/assets/skills/github.svg" },
+      { name: "Docker", icon: "/assets/skills/docker.svg" },
+      { name: "UV", icon: "/assets/skills/uv.svg" },
+      { name: "Anaconda", icon: "/assets/skills/anaconda.svg" },
+    ],
+  },
+];
